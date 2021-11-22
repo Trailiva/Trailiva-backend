27aa918a25adf6a2fa2baf060e2e7e28f5eff5be
whalewalker <mongodbms@gmail.com>
2021-11-22 16:04:39 +0100

    Setup simple Gradle Project

---
+++ src/main/java/com/trailiva/TrailivaApplication.java
@@ -0,0 +1,13 @@
+package com.trailiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trailiva/TrailivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trailiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
