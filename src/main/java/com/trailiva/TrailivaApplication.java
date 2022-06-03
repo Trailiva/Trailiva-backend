2b1756d3b68354e6c1b01ba2d836db3b21196c68
whalewalker <mongodbms@gmail.com>
2022-06-03 22:50:04 +0100

    setup project

--- src/main/java/com/trailiva/TrailivaApplication.java
+++ src/main/java/com/trailiva/TrailivaApplication.java
@@ -1,10 +1,7 @@
 package com.trailiva;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
 public class TrailivaApplication {
