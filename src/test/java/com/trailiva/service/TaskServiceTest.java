b7f04209968962fec905fd5ee44f8b55d75bf525
Adeola Oladeji <deolaoladeji@gmail.com>
2022-06-11 14:40:24 +0100

    Implementation of filter task by priority

--- src/test/java/com/trailiva/service/TaskServiceTest.java
+++ src/test/java/com/trailiva/service/TaskServiceTest.java
@@ -2,6 +2,7 @@ package com.trailiva.service;
 
 
 import com.trailiva.data.model.Priority;
+import com.trailiva.data.model.Task;
 import com.trailiva.data.repository.TaskRepository;
 import com.trailiva.web.exceptions.TaskException;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 @Slf4j
@@ -27,14 +31,26 @@ public class TaskServiceTest {
     @InjectMocks
     private TaskServiceImpl taskService;
 
+    private Task firstTask;
+    private Task secondTask;
     @BeforeEach
     void setUp(){
+        firstTask = new Task();
+        firstTask.setId(1L);
+        firstTask.setDescription("first task");
+        firstTask.setPriority(Priority.HIGH);
 
+        secondTask = new Task();
+        secondTask.setId(2L);
+        secondTask.setDescription("second task");
+        secondTask.setPriority(Priority.LOW);
     }
 
     @Test
     void testThatATaskCanBeFilteredByPriority() throws TaskException {
-        when(taskRepository.findAll()).thenReturn()
-        taskService.filterTaxByPriority(Priority.LOW);
+        List<Task> allTask = List.of(firstTask, secondTask);
+        when(taskRepository.findAll()).thenReturn(allTask);
+        List<Task> filteredTask = taskService.filterTaxByPriority(Priority.LOW);
+        assertThat(filteredTask.size()).isEqualTo(1);
     }
 }
