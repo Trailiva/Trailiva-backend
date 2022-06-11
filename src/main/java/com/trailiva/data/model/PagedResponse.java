19000115524add42dc84ea8e4ab9460a3acd3552
whalewalker <mongodbms@gmail.com>
2022-06-11 19:42:36 +0100

    TRAIL-54: created a paged model to map response with neccessary parameter

---
+++ src/main/java/com/trailiva/data/model/PagedResponse.java
@@ -0,0 +1,19 @@
+package com.trailiva.data.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagedResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
